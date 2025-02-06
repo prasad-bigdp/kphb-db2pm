64c9c661fe61e6e865d3b329e6f82de62416dbe6
prasad-bigdp <bigdp585@gmail.com>
2025-02-06 15:45:17 +0530

    update

---
+++ mySQL/06-02-25.sql
@@ -0,0 +1,32 @@
+-- 06/02/25
+-- create database school;
+-- use school;
+-- create table teachers(tid int,
+-- tname varchar(100),gender char(1),
+-- doj date);
+/* create database school;
+use school;
+create table teachers(tid int,
+tname varchar(30),gender char(1),
+doj date); */
+-- rename table teachers to faculty;
+-- adding a column
+-- alter table faculty 
+-- add tsalary decimal(10,2);
+-- change column name
+-- alter table faculty 
+-- change tsalary salary decimal(10,2)
+-- delete a column
+alter table faculty
+drop column salary;
+
+
+
+
+
+
+
+
+
+
+
